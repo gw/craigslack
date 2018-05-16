92c687f3d68aa28fc60810b21479b88223919a3b
Grant Wu <grantwu2@gmail.com>
2018-05-15 22:19:35 -0700

    Pipe STDERR to STDOUT in run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-cd ~/Code/git/craigslack && source ~/.virtualenvs/craigslack/bin/activate && python run.py >> /tmp/craigslack.log
+cd ~/Code/git/craigslack && source ~/.virtualenvs/craigslack/bin/activate && python run.py 2>&1 >> /tmp/craigslack.log
